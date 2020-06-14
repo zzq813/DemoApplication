5b39c81d9f7109067f88f50c69328658a2ff7406
zzq813 <zzq8139@126.com>
2020-06-14 23:53:31 +0800

    new

---
+++ GitHubApplication/GitHubApplication/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitHubApplication
+//
+//  Created by 周志青 on 2020/6/14.
+//  Copyright © 2020 black. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
