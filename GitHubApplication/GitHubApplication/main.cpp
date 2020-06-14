ef89ff0e3d1349de6f2579e6c70b6b1ad59d5852
zzq813 <zzq8139@126.com>
2020-06-14 23:56:48 +0800

    another

--- GitHubApplication/GitHubApplication/main.cpp
+++ GitHubApplication/GitHubApplication/main.cpp
@@ -7,9 +7,10 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n"<<endl;
     return 0;
 }
